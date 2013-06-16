968e001c604befa8dae2eb9f503b93fbea0dd853
Nathan Cartwright <fshost@yahoo.com>
2013-06-16 18:40:36 -0500

    updated tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,24 @@
+var fs = require('fs');
+var assert = require('assert');
+var githubContents = require(__dirname + '/..');
+
 describe('github-contents', function() {
-    var githubContents = require(__dirname + '/..');
 
     it('should fetch the contents of a file in a github repo', function(done) {
         githubContents('fshost/github-contents', 'index.js', function(err, contents) {
             if (err) throw err;
-            var expectedContents = require('fs').readFileSync(__dirname + '/../index.js').toString();
+            var expectedContents = fs.readFileSync(__dirname + '/../index.js').toString();
             assert.equal(contents, expectedContents);
+            done();
+        });
+    });
+
+    it('should fetch the contents of a nested file in a github repo', function(done) {
+        githubContents('fshost/github-contents', 'test/test.js', function(err, contents) {
+            if (err) throw err;
+            var expectedContents = fs.readFileSync(__dirname + '/test.js').toString();
+            assert.equal(contents, expectedContents);
+            done();
         });
     });
 
